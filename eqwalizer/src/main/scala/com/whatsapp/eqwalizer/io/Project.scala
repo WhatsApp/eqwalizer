4b2ca3b1dcf871bbcbedc8cdfe42a5c5b352b134
Victor Lanvin <vlanvin@meta.com>
2022-12-01 01:21:04 -0800

    Restrict files analyzed by ApplicationEnvListener
    Summary:
    If a file includes another file whose path cannot be relativized, the ApplicationEnvListener crashes with a runtime error.
    This provides a temporary fix by restricting the analysis to only traverse files whose path can be relativised.
    
    Reviewed By: ilya-klyuchnikov
    
    Differential Revision: D41619240
    
    fbshipit-source-id: 279667db95defc416ef6441367db9b2a42b2da8d

--- eqwalizer/src/main/scala/com/whatsapp/eqwalizer/io/Project.scala
+++ eqwalizer/src/main/scala/com/whatsapp/eqwalizer/io/Project.scala
@@ -15,31 +15,43 @@ object Project {
 
   val thirdPartySuffix = "/third-party"
 
-  def relativePath(file: String): String = {
+  def relativePathOpt(file: String): Option[String] = {
     val projectRoot = Paths.get(config.sourceRoot)
     val otpRoot = Paths.get(config.otpLibRoot)
     val path = Paths.get(file)
     if (path.isAbsolute) {
       val canonical = Paths.get(new File(file).getCanonicalPath)
       if (canonical.startsWith(projectRoot)) {
-        projectRoot.relativize(canonical).toString
+        Some(projectRoot.relativize(canonical).toString)
       } else if (canonical.startsWith(otpRoot)) {
-        s"/otp/${otpRoot.relativize(canonical)}"
+        Some(s"/otp/${otpRoot.relativize(canonical)}")
       } else if (canonical.toString().contains(thirdPartySuffix)) {
         // buck case, converts
         // /data/users/$user/whatsapp/server/buck-out/lsp/gen/waserver/f6717464dc059109/third-party/__proper__/proper/include/proper.hrl
         // into /third-party/__proper__/proper/include/proper.hrl
         val subPath = canonical.toString().split(thirdPartySuffix)(1)
-        s"$thirdPartySuffix$subPath"
+        Some(s"$thirdPartySuffix$subPath")
       } else {
         // $COVERAGE-OFF$
-        throw new IllegalStateException(s"cannot relativize the path: $file")
+        None
         // $COVERAGE-ON$
       }
-    } else if (new File(config.sourceRoot, file).exists()) file
+    } else if (new File(config.sourceRoot, file).exists()) Some(file)
     // $COVERAGE-OFF$
-    // The only case we know of when paths will be unknown is when .erls are compiled with +deterministic.
-    else s"<unknown>/$file"
+    else None
   }
   // $COVERAGE-ON$
+
+  def relativePath(file: String): String = {
+    val path = Paths.get(file)
+    relativePathOpt(file) match {
+      case Some(path) => path
+      case None       =>
+        // $COVERAGE-OFF$
+        if (path.isAbsolute) throw new IllegalStateException(s"cannot relativize the path: $file")
+        // The only case we know of when paths will be unknown is when .erls are compiled with +deterministic.
+        else s"<unknown>/$file"
+      // $COVERAGE-ON$
+    }
+  }
 }
