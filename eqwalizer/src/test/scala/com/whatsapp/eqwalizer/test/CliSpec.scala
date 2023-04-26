83e0298f5e842454a3ea8e2924389c0bd5ff5dbb
Ilya Klyuchnikov <ilyaklyuchnikov@meta.com>
2023-04-26 02:49:59 -0700

    decommission DiscardedSpecs analysis
    Reviewed By: VLanvin
    
    Differential Revision: D45271918
    
    fbshipit-source-id: 112fc8be5874d2ee08897efb53f299b1a3efed8d

--- eqwalizer/src/test/scala/com/whatsapp/eqwalizer/test/CliSpec.scala
+++ eqwalizer/src/test/scala/com/whatsapp/eqwalizer/test/CliSpec.scala
@@ -59,10 +59,6 @@ class CliSpec extends SnapshotSpec {
       checkAction(UnionsWithTypeVars.main(Array()), "unions_with_type_vars.cli")
     }
 
-    it("reports FIXME specs") {
-      checkAction(DiscardedSpecs.main(Array()), "discarded_specs.cli")
-    }
-
     it("reports clauses with repeated variable names") {
       checkAction(RepeatedVars.main(Array()), "repeated_vars.cli")
     }
