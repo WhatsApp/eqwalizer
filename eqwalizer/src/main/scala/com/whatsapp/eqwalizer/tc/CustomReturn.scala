49a05bea0f936300be71c6727ff3c0c39682870b
Ilya Klyuchnikov <ilyaklyuchnikov@meta.com>
2024-05-14 06:09:35 -0700

    CustomReturn for erlang:min/2
    Summary:
    It turns out that calls `erlang:min(X, Y)` when `X` is a number and `Y` is `number() | infinity` or `number() | undefined` are not so uncommon - and can be considered as an "Erlangisms".  Programmers rely on the fact that `min(1, infinity) -> 1` or `min(1, undefined) -> 1`.
    
    Very ad-hoc custom handling for such cases.
    
    Reviewed By: VLanvin
    
    Differential Revision: D57249552
    
    fbshipit-source-id: 6bedce5381d23d60b7d21c1b0a115102540c204e

--- eqwalizer/src/main/scala/com/whatsapp/eqwalizer/tc/CustomReturn.scala
+++ eqwalizer/src/main/scala/com/whatsapp/eqwalizer/tc/CustomReturn.scala
@@ -10,13 +10,14 @@ import scala.annotation.tailrec
 
 import com.whatsapp.eqwalizer.ast.Exprs.{AtomLit, Cons, Expr, Tuple}
 import com.whatsapp.eqwalizer.ast.RemoteId
-import com.whatsapp.eqwalizer.ast.Types.{BinaryType, Type, stringType}
+import com.whatsapp.eqwalizer.ast.Types._
 
 final class CustomReturn(pipelineContext: PipelineContext) {
-  private lazy val util = pipelineContext.util
+  private lazy val subtype = pipelineContext.subtype
   private lazy val custom: Set[RemoteId] =
     Set(
-      RemoteId("re", "replace", 4)
+      RemoteId("erlang", "min", 2),
+      RemoteId("re", "replace", 4),
     )
 
   def isCustomReturn(remoteId: RemoteId): Boolean =
@@ -24,6 +25,21 @@ final class CustomReturn(pipelineContext: PipelineContext) {
 
   def customizeResultType(remoteId: RemoteId, args: List[Expr], argTys: List[Type], resTy: Type): Type =
     remoteId match {
+      case RemoteId("erlang", "min", 2) =>
+        val List(argTy1, argTy2) = argTys
+        if (subtype.isNoneType(argTy1) || subtype.isNoneType(argTy2))
+          resTy
+        else if (
+          subtype.gradualSubType(argTy1, NumberType) &&
+          subtype.gradualSubType(argTy2, UnionType(Set(NumberType, AtomType)))
+        )
+          NumberType
+        else if (
+          subtype.gradualSubType(argTy2, NumberType) &&
+          subtype.gradualSubType(argTy1, UnionType(Set(NumberType, AtomType)))
+        )
+          NumberType
+        else resTy
       case RemoteId("re", "replace", 4) =>
         args match {
           case List(_, _, _, options) =>
