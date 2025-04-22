96515fb534f4db449b733f7f6e15533586b1b11e
Victor Lanvin <vlanvin@meta.com>
2025-04-22 08:07:07 -0700

    Add copyright header to ELPProxy.scala
    Summary: As title.
    
    Reviewed By: sopotc
    
    Differential Revision: D73437498
    
    fbshipit-source-id: 44bfe74cd6ffb10232af7a356c1d04bec1f6ca4f

--- eqwalizer/src/main/scala/com/whatsapp/eqwalizer/ELPProxy.scala
+++ eqwalizer/src/main/scala/com/whatsapp/eqwalizer/ELPProxy.scala
@@ -1,3 +1,9 @@
+/* Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
 package com.whatsapp.eqwalizer
 
 import scala.collection.mutable
